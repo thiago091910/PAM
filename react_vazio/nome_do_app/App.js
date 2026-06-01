6b6afacba9fa4c398973d3ea669b7642e4e1128a
thiago091910 <thiagolimargs@gmail.com>
2026-06-01 12:31:32 +0000

    versao2

--- react_vazio/nome_do_app/App.js
+++ react_vazio/nome_do_app/App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <ScrollView>
     <View style={styles.container}>
-      <Text>Tela Inicial</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margintop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
