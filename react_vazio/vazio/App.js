11dbadcaf1c343795c0820b327e911d741e5b1dc
thiago091910 <thiagolimargs@gmail.com>
2026-06-02 17:58:18 +0000

    versao3

--- react_vazio/vazio/App.js
+++ react_vazio/vazio/App.js
@@ -1,23 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  return(
     <ScrollView>
-    <View style={styles.container}>
-      <Text>Tela Inicial</Text>
-      <StatusBar style="auto" />
-    </View>
-    </ScrollView>
-  );
+      <View style={styles.titulo}>
+        <Text>Teste de tela</Text>
+      </View>
+    <ScrollView style={styles.cont_texto}>
+      <Text>jtrfjrrtutr
+
+      </Text>
+      </ScrollView>
+      <View>
+        <TextInput style= {{height:40, borderColor: "blue", borderWidth: 2, width: 200,}}>
+
+        </TextInput>
+  
+</View>
+</ScrollView>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margintop: 50,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ titulo: {
+  flex: 1,
+  margintop:50,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign:'center',
+ },
+ cont_texto:{
+  margintop:50,
+  height:500,
+  width:300,
+  backgroundColor: "red",
+  alignSelf: 'center',
+ },
 });
